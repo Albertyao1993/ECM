b288a70a0c54a3a3a331c63fc0153f87b849901f
Guoqiang Yao <guoqiang_yao@outlook.com>
2024-09-16 16:46:35 +0200

    yolo live detect number of person in real time

--- backend/main.py
+++ backend/main.py
@@ -1,31 +1,34 @@
 # app.py
-from flask import Flask, jsonify
-from flask_socketio import SocketIO
-from flask_cors import CORS
+import time
 from threading import Thread
 from queue import Queue
+from flask import Flask, jsonify, request, Response
+from flask_socketio import SocketIO
+from flask_cors import CORS
+
 from Database.db_operation import Database
 from server.dth111 import DTH111
-import time
+from YOLO.video_detection import VideoDetection
 
 app = Flask(__name__)
 CORS(app, origins="*")
-socketio = SocketIO(app, cors_allowed_origins="*")  # 启用 CORS 允许前端访问
+socketio = SocketIO(app, cors_allowed_origins="*")  # Enable CORS for frontend access
 
 data_queue = Queue()
 
-# 初始化数据库
+# Initialize the database
 db = Database(uri="mongodb://localhost:27017/", db_name="sensor_data", collection_name="readings")
 dth111 = DTH111(socketio=socketio, data_queue=data_queue)
+video_detection = VideoDetection()
 
 def websocket_thread():
-    # 创建传感器模拟器实例
+    # Start reading sensor data
     dth111.read_sensor_data()
 
 def database_thread():
     while True:
         try:
-            # 每隔1分钟插入当前时刻的数据
+            # Insert the current data into the database every minute
             time.sleep(60)
             if not data_queue.empty():
                 latest_data_point = data_queue.get()
@@ -34,21 +37,28 @@ def database_thread():
         except Exception as e:
             print(f"Error in database thread: {e}")
 
-# 启动 WebSocket 线程
+# Start the WebSocket thread
 ws_thread = Thread(target=websocket_thread)
 ws_thread.daemon = True
 ws_thread.start()
 
-# 启动数据库线程
+# Start the database thread
 db_thread = Thread(target=database_thread)
 db_thread.daemon = True
 db_thread.start()
 
+# Start the YOLOv8 detection thread
+video_detection.start_detection()
+
 @app.route('/data', methods=['GET'])
 def get_data():
-    # 返回当前存储的所有数据
-    # return jsonify(sensor_simulator.get_data())
+    # Return the current stored data
     return jsonify(dth111.get_data())
 
+
+
 if __name__ == '__main__':
-    socketio.run(app, debug=True, host='127.0.0.1', port=5000, use_reloader=False, log_output=True)
+    try:
+        socketio.run(app, debug=True, host='127.0.0.1', port=5000, use_reloader=False, log_output=True)
+    finally:
+        video_detection.stop_detection()
